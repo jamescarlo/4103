e715184ce7c56af8057d9dac948cd4415e03f331
sniper4627 <joshua_apellido@yahoo.com>
2018-05-28 20:23:44 +0800

    :+1:

--- griffon-app/conf/Config.java
+++ griffon-app/conf/Config.java
@@ -12,7 +12,7 @@ public class Config extends AbstractMapResourceBundle {
         map(entries)
             .e("application", map()
                 .e("title", "griffon-app")
-                .e("startupGroups", asList("login"))
+                .e("startupGroups", asList("register"))
                 .e("autoShutdown", false)
             )
             .e("mvcGroups", map()
